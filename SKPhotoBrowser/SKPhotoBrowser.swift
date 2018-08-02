b1c4f23adb15143a23b760c49e8c946dd5b5d142
Marius Undrum <undrum@gmail.com>
2018-08-02 19:45:35 +0200

    Added upvote user message

--- SKPhotoBrowser/SKPhotoBrowser.swift
+++ SKPhotoBrowser/SKPhotoBrowser.swift
@@ -340,6 +340,13 @@ public extension SKPhotoBrowser {
         }
     }
     
+    func showMessageBox(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
+        print("Alert showing")
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     func getCurrentPageIndex() -> Int {
         return currentPageIndex
     }
@@ -500,6 +507,7 @@ internal extension SKPhotoBrowser {
     
     @objc func upvoteButtonPressed() {
         delegate?.pressedUpvoteButton?(currentPageIndex)
+        showMessageBox(title:"Takk for at du liker bildet", message: "")
     }
     
     func deleteImage() {
